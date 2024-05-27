1eeaa085573b250be3d9f812b8e224248c2e5c10
Vasyl Vintoniak <vvintonyak31@gmail.com>
2024-05-27 17:22:52 +0300

    Create utils that display dialogues for opening and saving files

---
+++ include/Utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+#include <string>
+#include <locale>
+#include <codecvt>
+
+class Utils
+{
+public:
+    static std::wstring OpenFileSelectionDialog(DWORD flags, LPCWSTR title, LPCWSTR filter = L"All Files\0*.*\0");
+    static std::wstring SaveFileSelectionDialog(DWORD flags, LPCWSTR title, LPCWSTR filter = L"All Files\0*.*\0");
+    static std::string wstring_to_utf8(const std::wstring& str);
+    static std::wstring utf8_to_wstring(const std::string& str);
+};
