14f45ca3316e86359b551bd1d04b10bd7acd8b5e
Vasyl Vintoniak <vvintonyak31@gmail.com>
2024-05-27 19:40:09 +0300

    saved

--- include/Scenario/Scenario.h
+++ include/Scenario/Scenario.h
@@ -1,17 +1,29 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include <string>
 #include "ScenarioStep.h"
 
 class Scenario
 {
 public:
+    Scenario();
+    Scenario(const std::string& name, const std::string& description);
+
     void addStep(std::shared_ptr<ScenarioStep> step);
 
     bool execute();
 
     const std::vector<std::shared_ptr<ScenarioStep>>& getSteps() const { return steps; }
+    void setName(const std::string& name);
+    std::string getName() const;
+
+    void setDescription(const std::string& description);
+    std::string getDescription() const;
 
 private:
+    std::string generateUniqueScenarioName() const;
     std::vector<std::shared_ptr<ScenarioStep>> steps;
+    std::string name;
+    std::string description;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,88 @@
-#include "UserInterface.h"
+//#include "UserInterface.h"
+//#include "Scenario/ScenarioManager.h"
+//#include "Conditional/TimeCondition.h"
+//#include "Conditional/BatteryLevelCondition.h"
+//#include "Task/MessageBoxTask.h"
+//#include "Task/ChangePowerPlanTask.h"
+//
+//int main()
+//{
+//    /*   UserInterface ui;
+//       ui.showMainMenu();*/
+//
+//    auto scenario1 = std::make_shared<Scenario>("Helloo", "Neeed more");
+//
+//    auto step1 = std::make_shared<ScenarioStep>(std::make_unique<BatteryLevelCondition>(60),  // USER SETS CURCIAL, CURRENT == 50.
+//        std::make_unique<ChangePowerPlanTask>());
+//    scenario1->addStep(step1);
+//
+//    auto step2 = std::make_shared<ScenarioStep>(std::make_unique<TimeCondition>(22),  // if not CURRENT TIME HOURS NUMBER yet perform box
+//        std::make_unique<MessageBoxTask>(), ExecutionTypeCondition::SUCCESS);
+//    scenario1->addStep(step2);
+//
+//
+//
+//    scenario1->execute();
+//
+//    return 0;
+//}
+
+#include <iostream>
+#include "FSManager.h"
+#include "Scenario/Scenario.h"
+#include "Scenario/ScenarioManager.h"
+#include "Conditional/TimeCondition.h"
+#include "Conditional/BatteryLevelCondition.h"
+#include "Task/MessageBoxTask.h"
+#include "Task/ChangePowerPlanTask.h"
 
 int main()
 {
-    UserInterface ui;
-    ui.showMainMenu();
+    // Створення об'єкту FSManager
+    FSManager fsManager;
+
+    // Створення сценарію
+    //std::string scenarioName = "SampleScenario";
+    //std::string scenarioDescription = "This is a sample scenario.";
+
+    auto scenarioManager = std::make_unique<ScenarioManager>();
+
+    //auto scenario1 = std::make_shared<Scenario>(scenarioName, scenarioDescription);
+    //auto scenario2 = std::make_shared<Scenario>();
+    //auto scenario3 = std::make_shared<Scenario>();
+
+    //auto step1 = std::make_shared<ScenarioStep>(std::make_unique<BatteryLevelCondition>(60),  // USER SETS CURCIAL, CURRENT == 50.
+    //    std::make_unique<ChangePowerPlanTask>());
+    //scenario1->addStep(step1);
+
+    //auto step2 = std::make_shared<ScenarioStep>(std::make_unique<TimeCondition>(17),  // if not CURRENT TIME HOURS NUMBER yet perform box
+    //    std::make_unique<MessageBoxTask>(), ExecutionTypeCondition::FAILURE);
+    //scenario2->addStep(step2);
+    ////scenarioManager->addScenario(scenario1);
+    ////scenarioManager->addScenario(scenario2);
+    //scenarioManager->addScenario(scenario3);
+    //scenarioManager->executeScenarios();
+
+    //fsManager.saveScenarios(*scenarioManager, "D:\\UniversityKeep\\ContextManagementSystem\\.DB\\blink.json");
+
+    //auto scenarioManagerEX = std::make_unique<ScenarioManager>();
+    auto scenarioManagerEX = std::make_unique<Scenario>();
+    fsManager.loadScenarios(*scenarioManagerEX, "D:\\UniversityKeep\\ContextManagementSystem\\.DB\\blink.json");
+
+    scenarioManagerEX->execute();
+    //auto scenario3 = std::make_shared<Scenario>();
+    //fsManager.loadScenarioDetails(*scenario3, "D:\\UniversityKeep\\ContextManagementSystem\\.DB\\TEST-TEst-new.json");
+    //scenario3->execute();
+    //std::cout << scenario3->getDescription();
+
+    //std::cout << std::endl;
+    //std::cout << scenario3->getName();
+    //std::cout << std::endl;
+    //scenarioManager->addScenario(scenario3);
+
+    //fsManager.saveScenarioDetails(*scenario3, "D:\\UniversityKeep\\ContextManagementSystem\\.DB\\TEST-TEst-new.json");
+    //fsManager.saveScenario(*scenario1, "D:\\UniversityKeep\\ContextManagementSystem\\.DB\\TEST-old.json");
+ 
 
     return 0;
 }
